268b060e8009ecaa74f9644ea59c0af660070d6a
murray-vm <murray@locallizard.co.za>
2024-08-07 13:43:33 +0200

    Add new grid button
    Button that asks user for new grid size and creates said grid

--- js-script.js
+++ js-script.js
@@ -1,4 +1,5 @@
 const containerDiv = document.querySelector(".container")
+const button = document.querySelector("button")
 
 function hoverChange(e) {
     console.log("hover")
@@ -6,25 +7,33 @@ function hoverChange(e) {
     block.classList.add("coloredBlock")
 }
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div")
         row.classList.add("row")
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const block = document.createElement("div") 
             block.classList.add("block")
             
             block.addEventListener("mouseover", hoverChange)
             
             row.appendChild(block)
-
-
-
-
         }
         containerDiv.appendChild(row)
     }
 }
+
+function newGrid() {
+    gridSize = prompt("Enter the size of your grid")
+    if (gridSize > 100 || gridSize < 1) 
+        alert("grid size invalid please keep the grid size between 1 and 100")
+    else {
+        containerDiv.textContent = ""
+        createGrid(gridSize)
+    }
+}
+
+button.addEventListener("click", newGrid)
 
 createGrid()
