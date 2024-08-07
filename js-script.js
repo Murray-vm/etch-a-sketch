b758e44c3bc564096069bb5191ad1b62d4c573ae
murray-vm <murray@locallizard.co.za>
2024-08-07 13:32:16 +0200

    Add hover function
    Blocks change color when mouse hovers over them

--- js-script.js
+++ js-script.js
@@ -1,5 +1,11 @@
 const containerDiv = document.querySelector(".container")
 
+function hoverChange(e) {
+    console.log("hover")
+    const block = e.target
+    block.classList.add("coloredBlock")
+}
+
 function createGrid() {
     for (let i = 0; i < 16; i++) {
         const row = document.createElement("div")
@@ -8,7 +14,14 @@ function createGrid() {
         for (let j = 0; j < 16; j++) {
             const block = document.createElement("div") 
             block.classList.add("block")
+            
+            block.addEventListener("mouseover", hoverChange)
+            
             row.appendChild(block)
+
+
+
+
         }
         containerDiv.appendChild(row)
     }
